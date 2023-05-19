671cb79b078df921f2fe0733c35f5e2507503048
LorisMarzocchi <ml77182a@gmail.com>
2023-05-19 14:48:31 +0200

    createindex

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+import Items from "./components/Items.vue";
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+    Items,
+  },
+};
+</script>
+
+<template>
+  <Header></Header>
+  <Main>
+    <Items></Items>
+  </Main>
+  <Footer></Footer>
+</template>
+
+<style lang="scss">
+@use "./assets/style/general.scss" as *;
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+</template>
+
+<style scoped>
+img {
+  height: 100px;
+  width: 100px;
+  background-color: black;
+}
+</style>
